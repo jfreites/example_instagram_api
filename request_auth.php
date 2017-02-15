396ac0252bb3b08bb08ecc73d9c202be1e354e30
Jonathan Freites <jfreites@users.noreply.github.com>
2017-02-15 06:41:17 -0600

    Remove CLIENTE ID

--- request_auth.php
+++ request_auth.php
@@ -3,7 +3,7 @@
 $authEndpoint = 'https://api.instagram.com/oauth/authorize/?client_id=%s&redirect_uri=%s&response_type=%s&scope=basic+public_content';
 
 $location = sprintf($authEndpoint,
-            '0e60bda7e475412188323fe22fde5f2b',
+            'your_client_id',
             'http://localhost:9000/confirm.php',
             'code');
 
